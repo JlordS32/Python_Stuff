790ab1585341096e088c211dce0d7ce2b4692459
JlordS32 <jlordsty128@gmail.com>
2022-05-16 20:18:03 +1000

    new files

--- experiments/h1/ichiban.py
+++ experiments/h1/ichiban.py
@@ -1,6 +1,10 @@
 import random
 import time
 
+print("Welcome to Ichiban Kuji!")
+
+seconds = 2
+
 A = []
 B = []
 C = []
@@ -31,20 +35,22 @@ figure_class = A + B + C + D
 
 result = A + B + C + D + E + F + G + H
 
-tickets = int(input("How many tickets? "))
+tickets = int(input("\nHow many tickets? "))
 
 print("\nHere are your prizes: ")
 print("----------------------")
 for i in range(tickets):
-    time.sleep(1)
+    time.sleep(seconds)
     x = random.choice(result)
     if x in figure_class:
         Figure += 1
     print(x)
 if Figure == 0:
     print("\nNo figures unfortunately, better try again next time.")
+elif Figure == 1:
+    print("\nCongratulations! You just won {} figure.".format(Figure))
 else:
-    print("\nCongratulation! You won {} figures".format(Figure))
+    print("\nCongratulations! You won {} figures.".format(Figure))
 
 
 
