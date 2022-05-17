4c0b64b3a4c7fece5b0adedb3fe464320279735c
JlordS32 <jlordsty128@gmail.com>
2022-05-17 11:42:44 +1000

    new files

--- experiments/account setup/programming_assessment.py
+++ experiments/account setup/programming_assessment.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 WIDTH = 500
 HEIGHT = 300
 
@@ -12,8 +13,8 @@ class WindowConfig(tk.Tk):
         self.geometry("{}x{}".format(WIDTH, HEIGHT))
         self.resizable(False, False)
         self.title("GELOS Enterprise")
-        # self.grid_columnconfigure(0, weight=1)
-        # self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
 
 class App:
@@ -30,11 +31,13 @@ class App:
         self.file = None
 
         # FRAME
-        self.main_frame = tk.Frame(window)
-        self.signup_frame = tk.Frame(window)
+        self.signup_frame = tk.Frame(window, border=100)
+        self.main_frame = tk.Frame(window, border=50)
 
-        for frame in (self.main_frame, self.signup_frame):
-            frame.grid(row=0, column=0, sticky="nsew")
+        self.signup_frame.grid(row=0, column=0, sticky="nsew")
+        self.main_frame.grid(row=0, column=0, sticky="nsew")
+        self.main_frame.place_configure(rely=0.5, relx=0.5, anchor=tk.CENTER)
+        self.signup_frame.place_configure(rely=0.5, relx=0.5, anchor=tk.CENTER)
 
 
         # MAIN FRAME
@@ -56,7 +59,19 @@ class App:
         signup_btn.grid(columnspan=2, row=4)
 
         # SIGN UP FRAME
-        tk.Label(self.signup_frame, text='Page 1', font='times 35', bg='red')
+        main_btn = tk.Button(self.signup_frame, text="Create Account", command=lambda: self.click_sign_up(self.main_frame))
+        main_btn.grid(row=3, column=0, columnspan=2)
+
+        signup_text = tk.Label(self.signup_frame, text="Sign up", font="calibri 20")
+        signup_text.grid(row=0, column=0, columnspan=2)
+
+        tk.Label(self.signup_frame, text="Username").grid(row=1, column=0, padx=5, pady=5)
+        tk.Label(self.signup_frame, text="Password").grid(row=2, column=0, padx=5, pady=5)
+
+        tk.Entry(self.signup_frame, textvariable=self.entry_text, width=30, font="arial 10").grid(row=1, column=1,
+                                                                                                  padx=5, pady=5)
+        tk.Entry(self.signup_frame, textvariable=self.entry_text1, width=30, font="arial 10").grid(row=2, column=1,
+                                                                                                   padx=5, pady=5)
 
     def on_click(self):
         self.a = self.entry_text.get()
